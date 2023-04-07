aa290455ddcb072ab499216ae6babbbe1d7c94c5
Raja <rajael@example.com>
2023-04-07 07:22:16 -0700

    updatess

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
-		
 #include "main.h"
-		
 
-		
 /**
-		
- * main - prints the name of the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
+ * main - Prints the name of the program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
-		
  * Return: Always 0 (Success)
-		
  */
-		
-int main(int argc __attribute__((unused)), char *argv[])
-		
-{
-		
-	printf("%s\n", *argv);
-		
 
-		
-	return (0);
-		
+int main(int argc __attribute__((unused)), char *argv[])
+{
+        printf("%s\n", *argv);
+        return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,56 +2,65 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
+
+/**
+ * check_num - checks if a string only contains digits
+ *
+ * @str: string to be checked
+ *
+ * Return: 1 if string only contains digits, 0 otherwise
+ */
+int check_num(char *str)
 {
-		/*Declaring variables*/
-		unsigned int count;
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
-			count++;
-		}
-		return (1);
-	}
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
-	{
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
-		{
-			if (check_num(argv[count]))
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			count++;
-		}
-		printf("%d\n", sum); /*print sum*/
-		return (0);
-	}
+        unsigned int count;
+
+        count = 0;
+
+        while (count < strlen(str))
+        {
+                if (!isdigit(str[count]))
+                {
+                        return (0);
+                }
+                count++;
+        }
+
+        return (1);
+}
+
+/**
+ * main - calculates the sum of all the arguments that are numbers
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+        int count;
+        int str_to_int;
+        int sum;
+
+        count = 1;
+        sum = 0;
+
+        while (count < argc)
+        {
+                if (check_num(argv[count]))
+                {
+                        str_to_int = atoi(argv[count]);
+                        sum += str_to_int;
+                }
+                else
+                {
+                        printf("Error\n");
+                        return (1);
+                }
+                count++;
+        }
+
+        printf("%d\n", sum);
+
+        return (0);
+}

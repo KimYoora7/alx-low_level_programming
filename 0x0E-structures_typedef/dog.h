4171e626bb4e4cc4054f6b365306407248946c49
Raja <rajael@example.com>
2023-04-19 19:58:30 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,36 +1,86 @@
+/**
+
+    @file dog.h
+    This header file defines a struct for a dog's basic information and declares several functions to operate on the struct.
+    */
+
 #ifndef DOG_H
 #define DOG_H
 
+/**
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+    @struct dog
+    A struct that contains a dog's basic information.
+    @name: A pointer to the dog's name.
+    @age: A float representing the dog's age.
+    @owner: A pointer to the owner's name.
+    */
+    struct dog
+    {
+    char *name;
+    float age;
+    char *owner;
+    };
 
+/**
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
+    @typedef dog_t
+    A typedef for struct dog.
+    */
+    typedef struct dog dog_t;
 
+/**
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
+    @brief Initializes a dog struct.
+    @param d A pointer to the dog struct to be initialized.
+    @param name A pointer to a char array containing the dog's name.
+    @param age A float representing the dog's age.
+    @param owner A pointer to a char array containing the owner's name.
+    @return void
+    */
+    void init_dog(struct dog *d, char *name, float age, char *owner);
 
+/**
 
-	#endif
+    @brief Prints a dog's basic information.
+    @param d A pointer to the dog struct to be printed.
+    @return void
+    */
+    void print_dog(struct dog *d);
 
+/**
+
+    @brief Creates a new dog struct.
+    @param name A pointer to a char array containing the dog's name.
+    @param age A float representing the dog's age.
+    @param owner A pointer to a char array containing the owner's name.
+    @return A pointer to a newly created dog struct.
+    */
+    dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+
+    @brief Frees the memory allocated for a dog struct.
+    @param d A pointer to the dog struct to be freed.
+    @return void
+    */
+    void free_dog(dog_t *d);
+
+/**
+
+    @brief Copies the string pointed to by src to the buffer pointed to by dest.
+    @param dest A pointer to the destination buffer.
+    @param src A pointer to the source string.
+    @return A pointer to dest.
+    */
+    char *_strcpy(char *dest, char *src);
+
+/**
+
+    @brief Computes the length of the string s.
+    @param s A pointer to the string whose length is to be computed.
+    @return The length of the string.
+    */
+    int _strlen(char *s);
+
+#endif
